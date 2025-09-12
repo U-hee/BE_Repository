840dce31399dc10064205c903fbf21f58383ba6f
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2025-09-12 10:43:41 +0900

    Settings: initial commit

---
+++ src/test/java/com/imfine/ngs/NgsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imfine.ngs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
