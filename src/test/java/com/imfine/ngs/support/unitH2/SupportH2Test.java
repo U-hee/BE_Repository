054b4826aa181a50d7e524fe684359893d046d29
U-hee <uhee0112@gmail.com>
2025-09-15 18:04:10 +0900

    test: add support

--- src/test/java/com/imfine/ngs/support/unitH2/SupportH2Test.java
+++ src/test/java/com/imfine/ngs/support/unitH2/SupportH2Test.java
@@ -60,22 +60,7 @@ public class SupportH2Test {
     @DisplayName("User별 문의조회 가능하다.")
     void findByUserId() {
         long userId = 1L;
-
-        List<Support> s = new ArrayList<>();
-        for (int i = 0; i < 5; i++) {
-            s.add(Support.builder()
-                    .userId(1L)
-                    .orderId(5L)
-                    .categoryId(1L)
-                    .content("조회 테스트")
-                    .createdAt(LocalDateTime.now())
-                    .build()
-            );
-            supportRepo.save(s.get(i));
-
-        }
-
-
+        insertSupport(userId, 1L);
         List<Support> supports = supportService.findSupportByUserId(userId);
         assertThat(supports).isNotEmpty();
         supports.forEach(support -> assertThat(support.getUserId()).isEqualTo(userId));
@@ -85,22 +70,21 @@ public class SupportH2Test {
     @DisplayName("Category별 문의조회 가능하다.")
     void findByCategoryId() {
         long categoryId = 1L;
-
-        for (int i = 0; i < 5; i++) {
-            Support s = Support.builder()
-                    .userId(2L)
-                    .orderId(5L)
-                    .categoryId(1L)
-                    .content("조회 테스트" + i)
-                    .build();
-            supportRepo.save(s);
-            System.out.println("Saved id=" + s.getId() + ", categoryId=" + s.getCategoryId());
-
-        }
-
-
+        insertSupport(1L, categoryId);
         List<Support> supports = supportService.findSupportByCategoryId(categoryId);
         assertThat(supports).isNotEmpty();
         supports.forEach(support -> assertThat(support.getCategoryId()).isEqualTo(categoryId));
+    }
+
+    void insertSupport(long userId, long categoryId) {
+        for (int i = 0; i < 5; i++) {
+            Support s = Support.builder()
+                    .userId(userId)
+                    .orderId(5L)
+                    .categoryId(categoryId)
+                    .content("조회 테스트" + i)
+                    .build();
+            supportRepo.save(s);
+        }
     }
 }
