bf6e3c8c47932493eafb2b6e6f7301f296b9cc3d
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2025-09-16 16:35:46 +0900

    chore: delete comment

--- src/main/java/com/imfine/ngs/order/entity/Game.java
+++ src/main/java/com/imfine/ngs/order/entity/Game.java
@@ -11,7 +11,7 @@ import lombok.ToString;
 @Getter
 @ToString
 @AllArgsConstructor
-@NoArgsConstructor // Required for JPA Embeddable
+@NoArgsConstructor
 @EqualsAndHashCode
 public class Game {
     private String name;
