cfb57b392f78d29ad156be3b55fa210932acb444
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2025-09-17 18:43:09 +0900

    chore: delete unrelated config

--- src/test/java/com/imfine/ngs/NgsApplicationTests.java
+++ src/test/java/com/imfine/ngs/NgsApplicationTests.java
@@ -1,6 +1,5 @@
 package com.imfine.ngs;
 
-import com.imfine.ngs.payment.client.PortOneClient;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,15 +8,6 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootTest
 class NgsApplicationTests {
-
-    @TestConfiguration
-    static class TestConfig {
-        @Bean
-        public PortOneClient portOneClient() {
-            return Mockito.mock(PortOneClient.class);
-        }
-    }
-
     @Test
     void contextLoads() {
     }
