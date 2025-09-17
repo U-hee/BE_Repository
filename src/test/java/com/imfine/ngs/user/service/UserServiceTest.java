6f06abed7d9314cc88017af2b22cb967e5789a1a
doublejh0501 <164894007+doublejh0501@users.noreply.github.com>
2025-09-17 11:01:23 +0900

    소셜 로그인 ,회원 탈퇴,상세 조회, display추가

--- src/test/java/com/imfine/ngs/user/service/UserServiceTest.java
+++ src/test/java/com/imfine/ngs/user/service/UserServiceTest.java
@@ -6,6 +6,7 @@ import com.imfine.ngs.user.oauth.client.OauthClient;
 import com.imfine.ngs.user.oauth.dto.OauthUserInfo;
 import com.imfine.ngs.user.repository.UserRepository;
 import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -39,17 +40,20 @@ class UserServiceTest {
 
 
     @Test
+    @DisplayName("회원가입 성공")
     void signUpSuccess() {
         Long id = userService.signUp("a@b.com", "1234", "1234", "Hun");
         assertNotNull(id);
     }
 
     @Test
+    @DisplayName("회원가입 실패 - 비밀번호 불일치")
     void signUpFailPwdDontMatch() {
         assertThrows(IllegalArgumentException.class, () -> userService.signUp("a@b.com", "12345", "1234", "Hun" ));
     }
 
     @Test
+    @DisplayName("회원가입 실패 - 중복 이메일")
     void signUpFailDuplicateEmail() {
         userService.signUp("a@b.com", "1234", "1234", "Hun");
 
@@ -57,6 +61,7 @@ class UserServiceTest {
     }
 
     @Test
+    @DisplayName("로그인 성공")
     void signInSuccess() {
         userService.signUp("a@b.com", "1234", "1234", "Hun");
         User user = userService.signIn("a@b.com", "1234");
@@ -65,6 +70,7 @@ class UserServiceTest {
     }
 
     @Test
+    @DisplayName("로그인 실패 - 비밀번호 불일치")
     void signInFailPwdDontMatch() {
         userService.signUp("a@b.com", "1234", "1234", "Hun");
 
@@ -72,11 +78,13 @@ class UserServiceTest {
     }
 
     @Test
+    @DisplayName("로그인 실패 - 존재하지 않는 이메일")
     void signInFailEmailDontMatch() {
         assertThrows(IllegalArgumentException.class, () -> userService.signIn("hi", "1234"));
     }
 
     @Test
+    @DisplayName("비밀번호 변경 성공")
     void updatePwdSuccess() {
         userService.signUp("a@b.com", "1234", "1234", "Hun");
         User user = userService.signIn("a@b.com", "1234");
@@ -86,6 +94,7 @@ class UserServiceTest {
     }
 
     @Test
+    @DisplayName("비밀번호 변경 실패 - 이전 비밀번호 불일치")
     void updatePwdFailPwdDontMatch() {
         userService.signUp("a@b.com", "1234", "1234", "Hun");
         assertThrows(IllegalArgumentException.class, () ->
@@ -94,6 +103,7 @@ class UserServiceTest {
     }
 
     @Test
+    @DisplayName("닉네임 변경 성공")
     void updateNicknameSuccess() {
         userService.signUp("a@b.com", "1234", "1234", "Hun");
 
@@ -104,6 +114,7 @@ class UserServiceTest {
     }
 
     @Test
+    @DisplayName("프로필 조회 성공")
     void userDetailSuccess() {
         userService.signUp("a@b.com", "1234", "1234", "Hun");
 
@@ -112,12 +123,15 @@ class UserServiceTest {
         assertEquals("Hun", profile.getNickname());
     }
 
+    @Test
+    @DisplayName("프로필 조회 실패")
     void userDetailFail() {
         userService.signUp("a@b.com", "1234", "1234", "Hun");
         assertThrows(IllegalArgumentException.class, () -> userService.getUserProfile("c@d.com"));
     }
 
     @Test
+    @DisplayName("회원 삭제 성공")
     void deleteUserSuccess() {
         userService.signUp("a@b.com", "1234","1234", "Hun");
 
@@ -127,6 +141,7 @@ class UserServiceTest {
     }
 
     @Test
+    @DisplayName("회원 삭제 실패 - 존재하지 않는 이메일")
     void deleteUserFail() {
         userService.signUp("a@b.com", "1234", "1234", "Hun");
 
---
+++ src/test/java/com/imfine/ngs/user/service/UserSocialServiceTest.java
@@ -0,0 +1,78 @@
+package com.imfine.ngs.user.service;
+
+import com.imfine.ngs.user.entity.User;
+import com.imfine.ngs.user.oauth.client.OauthClient;
+import com.imfine.ngs.user.oauth.dto.OauthUserInfo;
+import com.imfine.ngs.user.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+import java.util.Optional;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+public class UserSocialServiceTest {
+
+    @InjectMocks
+    private UserService userService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private OauthClient oauthClient;
+
+    @Test
+    @DisplayName("소셜 로그인 - 신규 유저 생성")
+    void socialSignUpNewUser() {
+        OauthUserInfo info = new OauthUserInfo("a@b.com", "Hun");
+
+        given(oauthClient.getUserInfo("google", "token"))
+                .willReturn(info);
+        given(userRepository.findByEmail("a@b.com"))
+                .willReturn(Optional.empty());
+        given(userRepository.save(any(User.class)))
+                .willAnswer(invocation -> invocation.getArgument(0));
+
+        User user = userService.socialLogin("google", "token");
+
+        assertEquals("a@b.com", user.getEmail());
+        assertEquals("Hun", user.getNickname());
+
+    }
+
+    @Test
+    @DisplayName("소셜 로그인 - 기존 유저 로그인 성공")
+    void socialsignInSuccess() {
+        User socialUser = User.create("a@b.com", "SOCIAL", "Hun", null);
+
+        given(oauthClient.getUserInfo("google", "validtoken"))
+                .willReturn(new OauthUserInfo("a@b.com", "Hun"));
+        given(userRepository.findByEmail("a@b.com"))
+                .willReturn(Optional.of(socialUser));
+
+        User user = userService.socialLogin("google", "validtoken");
+
+        assertEquals(socialUser.getNickname(), user.getNickname());
+    }
+
+    @Test
+    @DisplayName("소셜 로그인 실패 - 잘못된 토큰")
+    void socialsignInFail() {
+        given(oauthClient.getUserInfo("google", "Invalidtoken"))
+                .willThrow(new IllegalArgumentException("Invalid token"));
+
+        assertThrows(IllegalArgumentException.class, () -> userService.socialLogin("google", "Invalidtoken"));
+
+    }
+}
