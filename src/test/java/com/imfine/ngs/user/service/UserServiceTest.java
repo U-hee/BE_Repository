e4090cd0c6270f9ecaf17cf626073544e888fdb8
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2025-09-17 18:38:31 +0900

    fix: Fix test environment DB config and incorrect tests

--- src/test/java/com/imfine/ngs/user/service/UserServiceTest.java
+++ src/test/java/com/imfine/ngs/user/service/UserServiceTest.java
@@ -7,12 +7,19 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @ActiveProfiles("test")
 @Transactional
+@TestPropertySource(properties = {
+        "spring.datasource.url=jdbc:h2:mem:testdb;MODE=MySQL;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password="
+})
 class UserServiceTest {
 
     @Autowired
@@ -89,4 +96,3 @@ class UserServiceTest {
 
 
 }
-
