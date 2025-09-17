37cb6a2283c5afd23464f52ff22ad155c6405f17
doublejh0501 <164894007+doublejh0501@users.noreply.github.com>
2025-09-17 18:18:30 +0900

    빈 주입

--- src/test/java/com/imfine/ngs/user/service/SocialServiceTest.java
+++ src/test/java/com/imfine/ngs/user/service/SocialServiceTest.java
@@ -10,6 +10,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 
 import java.util.Optional;
 
