32e022c58fe821759787a4ee3eb3bae7a2949ec8
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2025-09-18 23:56:59 +0900

    feat: Complete payment functionality and separate Payment entity

--- src/main/java/com/imfine/ngs/payment/entity/Payment.java
+++ src/main/java/com/imfine/ngs/payment/entity/Payment.java
@@ -6,15 +6,23 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "payments")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "order_id")
     private Order order;
 
     private long amount; // 결제 금액
 
+    @Column(unique = true)
     private String impUid; // 포트원 거래 ID
 
     public Payment(Order order, long amount, String impUid) {
